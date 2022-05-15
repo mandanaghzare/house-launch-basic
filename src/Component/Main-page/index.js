7400754706069929b05b1f0ca2b7617cc1c04e38
Mandana Z <mandana.ghzare@gmail.com>
2022-05-15 19:10:59 +0430

    edit

--- src/Component/Main-page/index.js
+++ src/Component/Main-page/index.js
@@ -6,17 +6,6 @@ import SimpleMap from './MapAmlak'
 
 
 const MainPage = (props) => {
-    const [offset, setOffset] = useState(0);
-
-    useEffect(() => {
-        const onScroll = () => setOffset(window.pageYOffset);
-        // clean up code
-        window.removeEventListener('scroll', onScroll);
-        window.addEventListener('scroll', onScroll, { passive: true });
-        return () => window.removeEventListener('scroll', onScroll);
-    }, []);
-
-    console.log(offset); 
     return ( 
         <div>
             <Header></Header>
