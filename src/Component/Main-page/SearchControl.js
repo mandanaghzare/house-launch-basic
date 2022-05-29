f51fee7e6707670d072dad6a6026e0bc7dab1d38
Mandana Z <mandana.ghzare@gmail.com>
2022-05-29 18:38:00 +0430

    edit

--- src/Component/Main-page/SearchControl.js
+++ src/Component/Main-page/SearchControl.js
@@ -1,17 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useMap } from "react-leaflet";
 import { GeoSearchControl } from "leaflet-geosearch";
 import "./react-leaflet-geosearch.css";
+import ReactDOM from 'react-dom';
 
 const SearchControl = (props) => {
   const map = useMap();
+  const [isActive, setActive] = useState('false');
+
+  useEffect(() => {
+    const mmm = document.getElementsByClassName("results")
+    const length = mmm.innerHtml
+    console.log(length)
+    mmm.addEventListener('click', function(){
+        setActive(!isActive);
+      });
+  }, []);
+
+
+
 
   useEffect(() => {
     const searchControl = new GeoSearchControl({
+      notFoundMessage: 'مکان مورد نظر یافت نشد',
       provider: props.provider,
       ...props,
     });
-    console.log(props)
     map.addControl(searchControl);
     return () => map.removeControl(searchControl);
   }, [map, props]);
--- src/Component/Main-page/header.js
+++ src/Component/Main-page/header.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import './style.css';
 import NeshanMap from './NeshanMap';
 import NavigateButton from './navigateButton';
@@ -8,38 +8,37 @@ import { BiBuildingHouse } from "react-icons/bi";
 
 
 const Header = (props) => {
-  const [isActive, setActive] = useState('false')
-  const goBot = () => {
-      setActive(!isActive);
-      console.log(isActive)
-  }
-    return ( 
-        <div>
-            <header className={!isActive ? "goBot" : null}>
-                <NeshanMap/>
-                <div className="backCover"></div>
-                <div className="headerContent">
-                    <div className="logo">
-                        <img src={require('../../Img/logo.png')} alt="logo" />
-                    </div>
-                    <div className="headerItems">
-                        <a href="">
-                            <p>پنل مدیریت</p>
-                            <span><MdAdminPanelSettings /></span>
-                        </a>
-                        <a href="">
-                            <p>درج آگهی</p>
-                            <span id="_header_items_adv"><FcAdvertising /></span>
-                        </a>
-                        <a href="">
-                            <p>خرید رهن اجاره</p>
-                            <span id="_header_items_adv"><BiBuildingHouse /></span>
-                        </a>
-                    </div>
+    const [isActive, setActive] = useState('false')
+    const goBot = () => {
+        setActive(!isActive);
+    }
+
+
+    return (
+        <header className={!isActive ? "goBot" : null}>
+            <NeshanMap/>
+            <div className="backCover"></div>
+            <div className="headerContent">
+                <div className="logo">
+                    <img src={require('../../Img/logo.png')} alt="logo" />
                 </div>
-                <NavigateButton data={goBot} />
-            </header>
-        </div>
+                <div className="headerItems">
+                    <a href="">
+                        <p>پنل مدیریت</p>
+                        <span><MdAdminPanelSettings /></span>
+                    </a>
+                    <a href="">
+                        <p>درج آگهی</p>
+                        <span id="_header_items_adv"><FcAdvertising /></span>
+                    </a>
+                    <a href="">
+                        <p>خرید رهن اجاره</p>
+                        <span id="_header_items_adv"><BiBuildingHouse /></span>
+                    </a>
+                </div>
+            </div>
+            <NavigateButton data={goBot} />
+        </header>
      );
 }
  
